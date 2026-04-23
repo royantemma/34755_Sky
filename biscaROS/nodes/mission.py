5052412142d94598022369b55a17e58dfecacbf9
Amaury Chevoir <amaury.chevoir@gmail.com>
2026-04-23 09:07:26 +0200

    new biscaROS architecture

---
+++ biscaROS/nodes/mission.py
@@ -0,0 +1,3 @@
+"""
+The mission node is responsible for executing the mission files, when called via MQTT. It receives the name of the mission (or task), find it in the mission folder (or task folder), and executes the corresponding Python file.
+"""
---
+++ biscaROS/nodes/vision/bw_threshold.py
@@ -0,0 +1,3 @@
+"""
+This node is just a test node to show how to add a vision node, and draw it in the stream. It applies a simple black and white threshold to the camera feed, and provides the data for the visual overlay to the stream.
+"""
